ac5b49fe90a3627c351e6e7bc8e13a6de646a50d
hayouung <hayouung@gmail.com>
2019-11-09 23:28:33 +0000

    add standard and custom command handler test

---
+++ test/commands/custom/custom-command.test.ts
@@ -0,0 +1,39 @@
+import { handleCustomCommand, customCommands, CustomCommand } from "../../../src/commands/custom/custom-command";
+import { Message, Client } from "discord.js";
+
+describe("custom command", () => {
+    const performSpy = jasmine.createSpy("iamtest.perform");
+    const testCustomCommand: CustomCommand = {
+        name: "iamtest",
+        perform: performSpy,
+        condition: (message: Message) => message.content === "test!"
+    };
+
+    beforeEach(() => {
+        spyOn(customCommands, "getCommands").and.returnValue([testCustomCommand]);
+    });
+
+    afterEach(() => {
+        performSpy.calls.reset();
+    });
+
+    it("should perform command if matching condition found", () => {
+        const message = {
+            content: "test!"
+        } as Message;
+
+        const client = {} as Client;
+        handleCustomCommand(message, client);
+        expect(performSpy).toHaveBeenCalled();
+    });
+
+    it("should not perform command if no matching condition found", () => {
+        const message = {
+            content: "test?"
+        } as Message;
+
+        const client = {} as Client;
+        handleCustomCommand(message, client);
+        expect(performSpy).not.toHaveBeenCalled();
+    });
+});
---
+++ test/commands/standard/standard-command.test.ts
@@ -0,0 +1,53 @@
+import {
+    handleStandardCommand,
+    standardCommands,
+    StandardCommand
+} from "../../../src/commands/standard/standard-command";
+import { Message, Client } from "discord.js";
+
+describe("standard command", () => {
+    const performSpy = jasmine.createSpy("iamtest.perform");
+    const testStandardCommand: StandardCommand = {
+        name: "iamtest",
+        perform: performSpy,
+        aliases: ["testiam"]
+    };
+
+    beforeEach(() => {
+        spyOn(standardCommands, "getCommands").and.returnValue([testStandardCommand]);
+    });
+
+    afterEach(() => {
+        performSpy.calls.reset();
+    });
+
+    it("should find command by name and perform", () => {
+        const message = {
+            content: "--iamtest"
+        } as Message;
+
+        const client = {} as Client;
+        handleStandardCommand(message, client);
+        expect(performSpy).toHaveBeenCalled();
+    });
+
+    it("should find command by alias and perform", () => {
+        const message = {
+            content: "--testiam"
+        } as Message;
+
+        const client = {} as Client;
+        handleStandardCommand(message, client);
+        expect(performSpy).toHaveBeenCalled();
+    });
+
+    it("should not perform command if no command found", () => {
+        const message = {
+            content: "--asdf"
+        } as Message;
+
+        const client = {} as Client;
+        handleStandardCommand(message, client);
+        expect(performSpy).not.toHaveBeenCalled();
+    });
+});
